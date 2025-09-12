aba597c1615564279dca85b34a13683211dce9f9
Ramon <ramonjd@gmail.com>
2025-09-12 12:56:36 +1000

    Updated the `isContainerInsertableToInWriteMode` function to include a condition that allows insertion when the block list is empty and the block to be inserted is a content block. Added corresponding tests to verify this behavior.

--- packages/block-editor/src/store/private-selectors.js
+++ packages/block-editor/src/store/private-selectors.js
@@ -19,6 +19,7 @@ import {
 	isNavigationMode,
 	getBlockRootClientId,
 	getBlockAttributes,
+	getBlockCount,
 } from './selectors';
 import {
 	checkAllowListRecursive,
@@ -99,15 +100,19 @@ export function isContainerInsertableToInWriteMode(
 	rootClientId
 ) {
 	const isBlockContentBlock = isContentBlock( blockName );
+	const isBlockListEmpty = ! getBlockCount( state, rootClientId );
 	const rootBlockName = getBlockName( state, rootClientId );
 	const isContainerContentBlock = isContentBlock( rootBlockName );
 	const isRootBlockMain = getSectionRootClientId( state ) === rootClientId;
 
 	// In write mode, containers shouldn't be inserted into unless:
-	// 1. they are a section root;
-	// 2. they are a content block and the block to be inserted is also content.
+	// 1. the entire block list is empty and the block to be inserted is also content;
+	// 2. they are a section root;
+	// 3. they are a content block and the block to be inserted is also content.
 	return (
-		isRootBlockMain || ( isContainerContentBlock && isBlockContentBlock )
+		( isBlockContentBlock && isBlockListEmpty ) ||
+		isRootBlockMain ||
+		( isContainerContentBlock && isBlockContentBlock )
 	);
 }
 
